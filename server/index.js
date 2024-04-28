b891db6a904cfde60f6952a974233851529a8b1d
CulturalProfessor <svinayak994@gmail.com>
2024-04-28 22:28:49 +0530

    cors 23

--- server/index.js
+++ server/index.js
@@ -14,51 +14,56 @@ const secretKey = process.env.VITE_SECRET_KEY;
 const origin = process.env.ORIGIN;
 const uri = process.env.MONGO_URI;
 const PORT = process.env.PORT || 5000;
-// const corsOptions = {
-//   origin: origin,
-//   credentials: false,
-//   optionSuccessStatus: 200,
-// };
+const corsOptions = {
+  origin: origin,
+  credentials: false,
+  optionSuccessStatus: 200,
+};
 const app = express();
 
-// app.use(cors(corsOptions));
+app.use(cors(corsOptions));
 // app.options("*", cors(corsOptions));
 // Set preflight
-app.options("*", (req, res) => {
-  console.log("preflight");
-  if (
-    req.headers.origin === origin &&
-    allowMethods.includes(req.headers["access-control-request-method"]) &&
-    allowHeaders.includes(req.headers["access-control-request-headers"])
-  ) {
-    console.log("pass");
-    return res.status(204).send();
-  } else {
-    console.log("fail");
-  }
-});
+// app.options("*", (req, res) => {
+//   console.log("preflight");
+//   if (
+//     req.headers.origin === origin &&
+//     allowMethods.includes(req.headers["access-control-request-method"]) &&
+//     allowHeaders.includes(req.headers["access-control-request-headers"])
+//   ) {
+//     console.log("pass");
+//     return res.status(204).send();
+//   } else {
+//     console.log("fail");
+//   }
+// });
 
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", origin);
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS,CONNECT,TRACE"
-  );
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "Content-Type, Authorization, X-Content-Type-Options, Accept, X-Requested-With, Origin, Access-Control-Request-Method, Access-Control-Request-Headers"
-  );
-  res.setHeader("Access-Control-Allow-Credentials", true);
-  res.setHeader("Access-Control-Allow-Private-Network", true);
-  //  Firefox caps this at 24 hours (86400 seconds). Chromium (starting in v76) caps at 2 hours (7200 seconds). The default value is 5 seconds.
-  res.setHeader("Access-Control-Max-Age", 7200);
+// app.use((req, res, next) => {
+//   res.setHeader("Access-Control-Allow-Origin", origin);
+//   res.setHeader(
+//     "Access-Control-Allow-Methods",
+//     "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS,CONNECT,TRACE"
+//   );
+//   res.setHeader(
+//     "Access-Control-Allow-Headers",
+//     "Content-Type, Authorization, X-Content-Type-Options, Accept, X-Requested-With, Origin, Access-Control-Request-Method, Access-Control-Request-Headers"
+//   );
+//   res.setHeader("Access-Control-Allow-Credentials", true);
+//   res.setHeader("Access-Control-Allow-Private-Network", true);
+//   //  Firefox caps this at 24 hours (86400 seconds). Chromium (starting in v76) caps at 2 hours (7200 seconds). The default value is 5 seconds.
+//   res.setHeader("Access-Control-Max-Age", 7200);
 
-  next();
-});
+//   next();
+// });
 
 app.use(ExpressMongoSanitize());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  next();
+});
 
 app.use("/", routes);
 app.listen(PORT, () => {
