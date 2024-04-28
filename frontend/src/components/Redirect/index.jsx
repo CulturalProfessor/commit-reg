4a771dc04daff3d401f44c8d997b546b47201b50
CulturalProfessor <svinayak994@gmail.com>
2024-04-28 23:56:47 +0530

    ui updates

--- frontend/src/components/Redirect/index.jsx
+++ frontend/src/components/Redirect/index.jsx
@@ -8,8 +8,10 @@ export default function Redirect() {
         <img src="../osslogo.png" className="redirect_logo" alt="logo" />
         <h2 className="formHeadingElement">Team OSS</h2>
       </div>
-      <h2>Thanks for Your Response</h2>
-      <h2>Online registrations was a success, please visit our help desk for workshop payment</h2>
+      <h2>
+        Your Online registrations was a success, please visit our help desk for
+        workshop payment
+      </h2>
       <img src="/success.png" id="redirectImage" />
     </div>
   );
